f8d1edefee90ffa73d140a9a72c5e977ffa40f86
Jendoki <69723669+Jendoki@users.noreply.github.com>
2023-03-24 16:23:17 +0100

    Fix

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,6 +1,6 @@
 function openLightbox(media, medias) {
-    const lightboxModel = lightboxFactory(media, medias)
-    const lightbox = lightboxModel.getLightboxDOM()
-    const modal = document.getElementById('lightbox_modal')
-    modal.appendChild(lightbox)
-  }
+  const lightboxModel = lightboxFactory(media, medias)
+  const lightbox = lightboxModel.getLightboxDOM()
+  const modal = document.getElementById('lightbox_modal')
+  modal.appendChild(lightbox)
+}
--- scripts/utils/likemedia.js
+++ scripts/utils/likemedia.js
@@ -1,29 +1,29 @@
 function handleLike(media, mediaLikes, likedMedia, unlikedMedia) {
-    if (media.status === 'not-liked') {
-      likeMedia(media, mediaLikes, likedMedia, unlikedMedia)
-    } else if (media.status === 'liked') {
-      unlikeMedia(media, mediaLikes, likedMedia, unlikedMedia)
-    }
+  if (media.status === 'not-liked') {
+    likeMedia(media, mediaLikes, likedMedia, unlikedMedia)
+  } else if (media.status === 'liked') {
+    unlikeMedia(media, mediaLikes, likedMedia, unlikedMedia)
   }
-  
-  function likeMedia (media, mediaLikes, likedMedia, unlikedMedia) {
-    media.likes += 1
-    media.status = 'liked'
-    mediaLikes.textContent = media.likes
-    likedMedia.style.display = 'block'
-    unlikedMedia.style.display = 'none'
-    const totalLikes = document.querySelector('#total-likes')
-    const newTotalLikes = parseInt(totalLikes.textContent) + 1
-    totalLikes.textContent = newTotalLikes
-  }
-  
-  function unlikeMedia (media, mediaLikes, likedMedia, unlikedMedia) {
-    media.likes -= 1
-    media.status = 'not-liked'
-    mediaLikes.textContent = media.likes
-    likedMedia.style.display = 'none'
-    unlikedMedia.style.display = 'block'
-    const totalLikes = document.querySelector('#total-likes')
-    const newTotalLikes = parseInt(totalLikes.textContent) - 1
-    totalLikes.textContent = newTotalLikes
-  }
+}
+
+function likeMedia (media, mediaLikes, likedMedia, unlikedMedia) {
+  media.likes += 1
+  media.status = 'liked'
+  mediaLikes.textContent = media.likes
+  likedMedia.style.display = 'block'
+  unlikedMedia.style.display = 'none'
+  const totalLikes = document.querySelector('#total-likes')
+  const newTotalLikes = parseInt(totalLikes.textContent) + 1
+  totalLikes.textContent = newTotalLikes
+}
+
+function unlikeMedia (media, mediaLikes, likedMedia, unlikedMedia) {
+  media.likes -= 1
+  media.status = 'not-liked'
+  mediaLikes.textContent = media.likes
+  likedMedia.style.display = 'none'
+  unlikedMedia.style.display = 'block'
+  const totalLikes = document.querySelector('#total-likes')
+  const newTotalLikes = parseInt(totalLikes.textContent) - 1
+  totalLikes.textContent = newTotalLikes
+}
